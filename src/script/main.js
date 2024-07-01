626f160728cf4371e4935960e53d88c0950711f5
Jean <jepaalgo00@gmail.com>
2024-07-01 09:28:28 -0400

    Faça requisições AJAX com fetch API
     Faça requisições AJAX com fetch API

--- src/script/main.js
+++ src/script/main.js
@@ -13,19 +13,41 @@ document.addEventListener("DOMContentLoaded", function(){
 */
 
 $(document).ready(function() {
+    $("#cep").mask("00000-000");
+
     $('#btn-buscar-cep').click(function(){
         const cep = $("#cep").val();
         const endpoint = `https://viacep.com.br/ws/${cep}/json/`;
+        const botao = $(this);
+        $(botao).find("i").addClass("d-none");
+        $(botao).find("span").removeClass("d-none");
 
-        $.ajax({
-            url: endpoint,
-            method: 'GET',
-            success: function(resposta) {
-                console.log(resposta);
-            },
-            error: function(err) {
-                console.error('Erro ao buscar CEP:', err);
-            }
-        });
-    });
-});
+        // $.ajax(endpoint).done(function(resposta){
+        //     const logradouro = resposta.logradouro;
+        //     const bairro = resposta.bairro;
+        //     const cidade = resposta.localidade;
+        //     const estado = resposta.uf;
+        //     const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+        //     $("#endereco").val(endereco);
+
+        //     $(botao).find("i").removeClass("d-none");
+        //     $(botao).find("span").addClass("d-none");
+        // })
+
+        fetch(endpoint).then(function(resposta){
+            return resposta.json()
+        })
+        .then(function(json){
+            const logradouro = json.logradouro;
+            const bairro = json.bairro;
+            const cidade = json.localidade;
+            const estado = json.uf;
+            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+            $("#endereco").val(endereco);
+
+            $(botao).find("i").removeClass("d-none");
+            $(botao).find("span").addClass("d-none");
+            
+        })
+    })
+})
