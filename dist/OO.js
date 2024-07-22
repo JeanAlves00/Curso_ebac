d3df32bb249de68e0a37e1c798eaac167329b22f
Jean <jepaalgo00@gmail.com>
2024-07-22 15:03:45 -0400

    1

---
+++ dist/OO.js
@@ -0,0 +1,48 @@
+"use strict";
+
+//function Pokemon (nomeDoPokemon, tipoDoPokemon) {	
+//    this.nome = nomeDoPokemon
+//    this.tipo = tipoDoPokemon
+//}
+
+//const pikachu = new Pokemon('Pikachu', 'Eletrico')
+
+class Pokemon {
+  #hp = 100; // atributo privado
+
+  constructor(nomeDoPokemon, tipoDoPokemon) {
+    this.nome = nomeDoPokemon;
+    this.tipo = tipoDoPokemon;
+  }
+  atacar(nomeDoAtaque) {
+    console.log(`${this.nome} usou ${nomeDoAtaque}`);
+  }
+  recebeuDano(dano) {
+    this.#hp -= 10;
+  }
+  exibeHP() {
+    console.log(this.#hp);
+  }
+}
+class Pikachu extends Pokemon {
+  constructor() {
+    super('Pikachu', 'Eletrico');
+  }
+  atacar() {
+    console.log(`${this.nome} usou choque do trovao`);
+  }
+}
+const pikachuDoAsh = new Pikachu();
+pikachuDoAsh.recebeuDano();
+console.log(pikachuDoAsh.hp);
+pikachuDoAsh.atacar();
+pikachuDoAsh.exibeHP();
+const pikachu = new Pokemon('Pikachu', 'Eletrico');
+
+//pikachu.atacar('Choque do Trovao');
+
+//pikachu.nome = 'Pikachu';
+//pikachu.Tipo = 'Eletrico'
+
+console.log(pikachu);
+console.log(pikachuDoAsh);
