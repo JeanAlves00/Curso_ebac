1b543b8c0be848ddfd867dabc0b23fc373f04271
Jean <154983968+JeanAlves00@users.noreply.github.com>
2024-08-19 14:31:07 -0400

    termino

---
+++ dist/aula.js
@@ -0,0 +1,47 @@
+"use strict";
+
+var alunos = [{
+  nome: 'João',
+  nota: 5
+}, {
+  nome: 'Sofia',
+  nota: 9
+}, {
+  nome: 'Paulo',
+  nota: 2
+}, {
+  nome: 'Miguel',
+  nota: 3
+}, {
+  nome: 'Lara',
+  nota: 4
+}, {
+  nome: 'Julia',
+  nota: 8
+}, {
+  nome: 'Lucas',
+  nota: 3
+}, {
+  nome: 'Jean',
+  nota: 5
+}, {
+  nome: 'laura',
+  nota: 7
+}, {
+  nome: 'Joaquim',
+  nota: 9
+}, {
+  nome: 'Ana',
+  nota: 10
+}, {
+  nome: 'Vinicius',
+  nota: 8
+}];
+function alunosAprovados(aunos) {
+  return aunos.filter(function (auno) {
+    return auno.nota >= 6;
+  });
+}
+;
+var aprovados = alunosAprovados(alunos);
+console.log(aprovados);
