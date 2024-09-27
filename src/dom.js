3d2f6f8bd90cb558f2a63c7be0c64fa420c921e7
Jean <154983968+JeanAlves00@users.noreply.github.com>
2024-09-27 13:48:39 -0400

    Integre TypeScript com DOM

--- src/dom.js
+++ src/dom.js
@@ -1,2 +1,4 @@
 "use strict";
 const campoNome = document.getElementById('nome');
+const formulario = document.getElementsByTagName('form');
+const botaoEnviar = document.getElementById('btn-enviar');
--- src/dom.ts
+++ src/dom.ts
@@ -1,1 +1,3 @@
 const campoNome = document.getElementById('nome');
+const formulario = document.getElementsByTagName('form');
+const botaoEnviar = document.getElementById('btn-enviar');
