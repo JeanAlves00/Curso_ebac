9d962cc5902aab4b8fc6b8be9ff7933387581dc8
Jean <154983968+JeanAlves00@users.noreply.github.com>
2024-09-27 14:30:03 -0400

    exercicio

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicar(30, 4));
+console.log(saudacao("jean"));
---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+console.log(multiplicar(30, 4));
+console.log(saudacao("jean")); 
