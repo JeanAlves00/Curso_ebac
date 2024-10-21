a5b087d188003b0efddcb69acfd358f3ec197a94
Jean <154983968+JeanAlves00@users.noreply.github.com>
2024-10-21 09:32:23 -0400

    Manipule estado e eventos

--- projeto-react/src/components/Formulario/index.jsx
+++ projeto-react/src/components/Formulario/index.jsx
@@ -1,17 +1,54 @@
+import { useState } from 'react';
+
 const Formulario = () => {
     let [materiaA, setMateriaA] = useState(0);
     let [materiaB, setMateriaB] = useState(0);
     let [materiaC, setMateriaC] = useState(0);
+    let [nome, setNome] = useState('');
 
+    const alteraNome = (evento) => {
+        //setNome(evento.target.value);
+        setNome(estadoAnterior => {
+            return evento.target.value;
+        });
+    }
+
+    const renderizaResultado = () => {
+        const soma = materiaA + materiaB + materiaC;
+        const media = soma / 3;
+
+        if (media >= 7) {
+            return <p>Olá {nome}, você foi aprovado!</p>;
+        } else {
+            return <p>Olá {nome}, você foi reprovado!</p>;
+        }
+    }
 
     return (
         <form>
-            <input type="number" placeholder="Nota matéria A" onChange={evento => setMateriaA(evento.target.value)}/>
-            <input type="number" placeholder="Nota matéria B" />
-            <input type="number" placeholder="Nota matéria C" />
-            <p>O aluno foi aprovado</p>
+            <input 
+                type="text" 
+                placeholder="Nome do aluno" 
+                onChange={e => alteraNome(e)}
+            />
+            <input 
+                type="number" 
+                placeholder="Nota matéria A" 
+                onChange={evento => setMateriaA(parseInt(evento.target.value))} 
+            />
+            <input 
+                type="number" 
+                placeholder="Nota matéria B" 
+                onChange={evento => setMateriaB(parseInt(evento.target.value))} 
+            />
+            <input 
+                type="number" 
+                placeholder="Nota matéria C" 
+                onChange={evento => setMateriaC(parseInt(evento.target.value))} 
+            />
+            {renderizaResultado()}
         </form>
-    )
+    );
 }
 
 export default Formulario;
